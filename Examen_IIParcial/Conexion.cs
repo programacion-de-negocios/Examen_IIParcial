f2e0319c08219b85b7fd68aedfbdad29d0697d3a
Ramón Ventura <50932514+Ramon-Ventura@users.noreply.github.com>
2019-07-08 13:24:01 -0600

    Add project files.

---
+++ Examen_IIParcial/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_IIParcial
+{
+    class Conexion
+    {
+        public static string Cn= @"server = (local)\sqlexpress; database = ERP; integrated security = True";
+
+    }
+}
---
+++ Examen_IIParcial/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//USANDO LOS NAMESPACES NECESARIOS
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Examen_IIParcial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection SqlCon;
+        public MainWindow()
+        {
+            InitializeComponent();
+            string ConnectionString = Conexion.Cn;
+            SqlCon = new SqlConnection(ConnectionString);
+        }
+
+        //METODO PARA MOSTRAR A LAS PERSONAS
+        private void MostrarPersonas()
+        {
+            try
+            {
+                string query = "SELECT * FROM Zoo.Animal";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, SqlCon);
+                using (adapter)
+                {
+                    DataTable TablaAnimales = new DataTable();
+                    adapter.Fill(TablaAnimales);
+                    LbAnimales.DisplayMemberPath = "Nombre";
+                    LbAnimales.SelectedValuePath = "Id_Animal";
+                    LbAnimales.ItemsSource = TablaAnimales.DefaultView;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
